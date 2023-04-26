1eb358a359c20c9f1bcaa7fb50c394aecbe6201e
Felipe <felipevoidela@gmail.com>
2023-04-26 12:59:43 -0300

    finalizando quiz

--- quiz.js
+++ quiz.js
@@ -7,12 +7,16 @@ let pergunta1 =(prompt("1) O vinho precisa ser guardado na horizontal ou na vert
 if (pergunta1 === "horizontal"){
       document.write("1) O vinho precisa ser guardado na horizontal ou na vertical?")
       document.write("<br><br>")
-      document.write("Resposta: Está correta pois é na ", pergunta1, " que se guarda o vinho.")
+      document.write("Sua respota foi: ", pergunta1)
+      document.write("<br><br>")
+      document.write("Resposta: Está correta pois é na ", pergunta1, " que se guardam os vinho.")
       document.write("<br><br>")
       document.write("<br><br>")
       
-}else{(pergunta1 === "vertical")
+}else{
       document.write("1) O vinho precisa ser guardado na horizontal ou na vertical?") 
+      document.write("<br><br>")
+      document.write("Sua respota foi: ", pergunta1)
       document.write("<br><br>")
       document.write("Resposta: Está errada, a resposta correta seria na horizontal.")
       document.write("<br><br>")
@@ -27,14 +31,18 @@ let pergunta2 =(prompt("2) O vinho precisa respirar ou ser oxigenado para o sabo
 if (pergunta2 === "sim"){
       document.write("2) O vinho precisa respirar ou ser oxigenado para o sabor suavizar?")
       document.write("<br><br>")
-      document.write("Resposta: Para o vinho suavizar ele precisa sim respirar ou ser oxigenado.")
+      document.write("Sua respota foi: ", pergunta2)
+      document.write("<br><br>")
+      document.write("Resposta: Correto pois para o vinho suavizar ele precisa sim respirar ou ser oxigenado.")
       document.write("<br><br>")
       document.write("<br><br>")
       
 }else{
       document.write("2) O vinho precisa respirar ou ser oxigenado para o sabor suavizar?") 
       document.write("<br><br>")
-      document.write("Resposta: Está errada pois para o sabor suavizar ele precisar respirar.")
+      document.write("Sua respota foi: ", pergunta2)
+      document.write("<br><br>")
+      document.write("Resposta: Está errado pois para o sabor suavizar ele precisar respirar.")
       document.write("<br><br>")
       document.write("<br><br>")
 }
@@ -48,12 +56,16 @@ let pergunta3 =(prompt("3) Um vinho pode durar 1 mês após ser aberto?"))
 if (pergunta3 === "não"){
       document.write("3) Um vinho pode durar 1 mês após ser aberto?")
       document.write("<br><br>")
+      document.write("Sua respota foi: ", pergunta3)
+      document.write("<br><br>")
       document.write("Resposta: Está correta, pois um vinho dura entre 5 a 7 dias após ser aberto.")
       document.write("<br><br>")
       document.write("<br><br>")
       
 }else{
       document.write("3) Um vinho pode durar 1 mês após ser aberto?") 
+      document.write("<br><br>")
+      document.write("Sua respota foi: ", pergunta3)
       document.write("<br><br>")
       document.write("Resposta: Está errado pois um vinho não dura 1 mês após ser aberto.")
       document.write("<br><br>")
@@ -69,12 +81,16 @@ let pergunta4 =(prompt("4) O vinho tinto é feito de qual fruta?"))
 if (pergunta4 === "uva"){
       document.write("4) O vinho tinto é feito de qual fruta?")
       document.write("<br><br>")
+      document.write("Sua respota foi: ", pergunta4)
+      document.write("<br><br>")
       document.write("Resposta: Está correto, pois o vinho tinto é feito de uva.")
       document.write("<br><br>")
       document.write("<br><br>")
       
 }else{
       document.write("4) O vinho tinto é feito de qual fruta?") 
+      document.write("<br><br>")
+      document.write("Sua respota foi: ", pergunta4)
       document.write("<br><br>")
       document.write("Resposta: Está errado, pois o vinho tinto é feito de uva.")
       document.write("<br><br>")
@@ -85,17 +101,21 @@ if (pergunta4 === "uva"){
 //pergunta 5
 
 
-let pergunta5 =(prompt("5) Quais são as duas principais classificações do vinho?"))
+let pergunta5 =(prompt("5) Quais são as duas principais classificações de vinho?"))
 
 if (pergunta5 === "seco e suave"){
-      document.write("5) Quais são as duas principais classificações do vinho?")
+      document.write("5) Quais são as duas principais classificações de vinho?")
+      document.write("<br><br>")
+      document.write("Sua respota foi: ", pergunta5)
       document.write("<br><br>")
       document.write("Resposta: Está correto, pois as principais classificações do vinho são seco e suave.")
       document.write("<br><br>")
       document.write("<br><br>")
       
 }else{
-      document.write("5) Quais são as duas principais classificações do vinho?") 
+      document.write("5) Quais são as duas principais classificações de vinho?") 
+      document.write("<br><br>")
+      document.write("Sua respota foi: ", pergunta5)
       document.write("<br><br>")
       document.write("Resposta: Errado, pois os dois principais vinhos são o seco e o suave.")
       document.write("<br><br>")
@@ -111,12 +131,16 @@ let pergunta6 =(prompt("6) Qual a uva mais platanda mundialmente?"))
 if (pergunta6 === "cabernet sauvignon"){
       document.write("6) Qual a uva mais platanda mundialmente?")
       document.write("<br><br>")
+      document.write("Sua respota foi: ", pergunta6)
+      document.write("<br><br>")
       document.write("Resposta: Correto, a uva mais planta atualmente é a Cabernet Sauvignon.")
       document.write("<br><br>")
       document.write("<br><br>")
       
 }else{
       document.write("6) Qual a uva mais platanda mundialmente?") 
+      document.write("<br><br>")
+      document.write("Sua respota foi: ", pergunta6)
       document.write("<br><br>")
       document.write("Resposta: Está errado, pois a uva mais plantada atualmente é a Cabernet Sauvignon.")
       document.write("<br><br>")
@@ -132,12 +156,16 @@ let pergunta7 =(prompt("7) Qual país é o maior produtor de vinho?"))
 if (pergunta7 === "italia"){
       document.write("7) Qual país é o maior produtor de vinho?")
       document.write("<br><br>")
+      document.write("Sua respota foi: ", pergunta7)
+      document.write("<br><br>")
       document.write("Resposta: Correto, a Itália é o país que mais produz vinho.")
       document.write("<br><br>")
       document.write("<br><br>")
       
 }else{
       document.write("7) Qual país é o maior produtor de vinho?") 
+      document.write("<br><br>")
+      document.write("Sua respota foi: ", pergunta7)
       document.write("<br><br>")
       document.write("Resposta: Errado, a Itália é a maior produtora de vinho.")
       document.write("<br><br>")
@@ -153,12 +181,16 @@ let pergunta8 =(prompt("8) Qual país produz o melhor vinho?"))
 if (pergunta8 === "frança"){
       document.write("8) Qual país produz o melhor vinho?")
       document.write("<br><br>")
+      document.write("Sua respota foi: ", pergunta8)
+      document.write("<br><br>")
       document.write("Resposta: Correto, a França produz o melhor vinho.")
       document.write("<br><br>")
       document.write("<br><br>")
       
 }else{
       document.write("8) Qual país produz o melhor vinho?") 
+      document.write("<br><br>")
+      document.write("Sua respota foi: ", pergunta8)
       document.write("<br><br>")
       document.write("Resposta: Errado, segundo a OIV (organização Internacional da Vinha e do Vinho) a França produz o melhor vinho.")
       document.write("<br><br>")
@@ -174,12 +206,16 @@ let pergunta9 =(prompt("9) Quanto mais velho o vinho melhor?"))
 if (pergunta9 === "sim"){
       document.write("9) Quanto mais velho o vinho melhor?")
       document.write("<br><br>")
+      document.write("Sua respota foi: ", pergunta9)
+      document.write("<br><br>")
       document.write("Resposta: Correto, pois quanto mais velho o vinho, mais tempo de maturação ele teve.")
       document.write("<br><br>")
       document.write("<br><br>")
       
 }else{
       document.write("9) Quanto mais velho o vinho melhor?") 
+      document.write("<br><br>")
+      document.write("Sua respota foi: ", pergunta9)
       document.write("<br><br>")
       document.write("Resposta: Errado, pois vinhos novos não tiveram tempo necessário de maturação sendo assim suas propriedades não estão perfeitas.")
       document.write("<br><br>")
@@ -195,12 +231,16 @@ let pergunta10 =(prompt("10) Quantas uva são necessárias para fazer uma garraf
 if (pergunta10 === "300"){
       document.write("10) Quantas uva são necessárias para fazer uma garrafa de vinho?")
       document.write("<br><br>")
+      document.write("Sua respota foi: ", pergunta10)
+      document.write("<br><br>")
       document.write("Resposta: Correto, são necessárias em média 300 uvas para fazer uma garrafa de vinho.")
       document.write("<br><br>")
       document.write("<br><br>")
       
 }else{
       document.write("10) Quantas uva são necessárias para fazer uma garrafa de vinho?") 
+      document.write("<br><br>")
+      document.write("Sua respota foi: ", pergunta10)
       document.write("<br><br>")
       document.write("Resposta: Errado, são necessárias em média 300 uvas para fazer uma garrafa de vinho.")
       document.write("<br><br>")
